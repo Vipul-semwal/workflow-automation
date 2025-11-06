710a08495c4afa14d529cff5403d562868f66ed3
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-11-06 20:30:31 +0300

    adding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-"use client";
-
-import { useQuery } from "@tanstack/react-query";
-import { useTRPC } from "@/trpc/client";
-
-export default function Home() {
-    const trpc = useTRPC();
-    const { data: users } = useQuery(trpc.hello.queryOptions({ text: "bro" }));
-
-    console.log('data:', users);
-
-    return (
-        <div className="min-h-screen min-w-screen flex items-center justify-center">
-            {JSON.stringify(users)}
-        </div>
-    );
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { getQueryClient, trpc } from '~/trpc/server';
+import { ClientGreeting } from './client-greeting';
+export default async function Home() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.hello.queryOptions({
+      /** input */
+    }),
+  );
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div>...</div>
+      {/** ... */}
+      <ClientGreeting />
+    </HydrationBoundary>
+  );
 }
-
