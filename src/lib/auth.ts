2232fc274048b61d7c84121e9255ea627c55a800
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-11-07 01:44:54 +0300

    database file changed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
+import prisma from "@lib/database"
+
 export const auth = betterAuth({
   //...
 });
