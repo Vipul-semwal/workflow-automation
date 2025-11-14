aaa446bd38c3c6a9ec89655abfd8b9e15d6ddba9
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-11-15 01:08:09 +0300

    layout file for auth

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+
+
+
+const Layout = ({children}:{children:React.ReactNode})=>{
+    return (
+        <>
+                
+        </>
+    )
+}
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,21 @@
 import { LoginForm } from "@/features/auth/components/login-from";
 import {requireUnauth} from "@/lib/auth-utils";
+import Link from "next/link";
+import Image from "next/image"
 
 const page = async  ()=>{
     await requireUnauth() 
     return (
-        <div>
-         <LoginForm/>
-        </div>
+        <div className="bg-muted flex min-h-svh flex-col justify-center gap-6 p-6 md:p-10">
+            <div className="flex w-full max-w-s flex-col gap-6">
+                <Link href="/" className="flex items-center gap-2 self-center font-medium">
+                    <Image src="/logo/logo.svg" alt="logo" width={30} height={30}/>
+
+                </Link>
+<LoginForm/>
+
+            </div>
+                 </div>
     )
 };
 
