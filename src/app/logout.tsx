89444ab043d4e107357ac76fec622c49418c3c27
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-11-14 17:51:26 +0300

    logout button added

---
+++ src/app/logout.tsx
@@ -0,0 +1,15 @@
+"use client"
+import {Button} from "@/components/ui/button"
+import {authClient} from "@/lib/auth-client"
+
+
+export default function Logout (){
+
+    return (
+        <Button onClick={()=>{
+            authClient.signOut()
+            }}>
+        Log Out
+        </Button>
+    ) 
+}
