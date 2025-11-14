c0a57cce5863ec404d1d0c2479db3133ac78af8a
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-11-15 02:02:00 +0300

    improved auth screens

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,17 +6,8 @@ import Image from "next/image"
 const page = async  ()=>{
     await requireUnauth() 
     return (
-        <div className="bg-muted flex min-h-svh flex-col justify-center gap-6 p-6 md:p-10">
-            <div className="flex w-full max-w-s flex-col gap-6">
-                <Link href="/" className="flex items-center gap-2 self-center font-medium">
-                    <Image src="/logo/logo.svg" alt="logo" width={30} height={30}/>
-
-                </Link>
-<LoginForm/>
-
-            </div>
-                 </div>
-    )
+             <LoginForm/>
+           )
 };
 
 
