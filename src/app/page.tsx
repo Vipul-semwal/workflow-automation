1c785148fa2064ae65620710d94de66e56d51337
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-11-16 01:22:10 +0300

    trpc function workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,31 @@
+"use client"
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { getQueryClient, trpc } from '@/trpc/server';
 import { ClientGreeting } from './client';
 import {requireAuth} from "@/lib/auth-utils"
 import { caller  } from "@/trpc/server";
 import Logout from "./logout"
+import {useQuery, useMutation} from "@tanstack/react-query";
+import {useTRPC} from "@/trpc/client";
+import {Button} from "@/components/ui/button"
+ 
  
 export default async  function Home() {
-    await requireAuth();
+ const trpc = useTRPC()
+    const {data} = useQuery(trpc.getWorkflows.queryOptions());
+    const create = useMutation(trpc.createWorkflow.mutationOptions())
+   
 
-    const data = await caller.hello({text:"vipul sarkar"})
-  
+     
   return (
        <div className='flex min-h-screen min-w-screen justify-center  flex items-center'>
         <h1>Protected Route</h1>
            {JSON.stringify(data)}
            <Logout/>
+           <Button disabled={create.isPending}  onClick={()=>{
+           create.mutate()
+           }}></Button>
          </div>
   );
 }
+
